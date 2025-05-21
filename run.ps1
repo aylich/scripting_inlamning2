59dff53ae3c06532efcfe59bac749522e56cb63c
PontusYdstromStudent <pontus.ydstrom.student@ksgyf.se>
2025-05-21 11:16:56 +0200

    Initial edits to file run.ps1

---
+++ run.ps1
@@ -0,0 +1,2 @@
+Write-Host "Hello, please enter your zodiac sign to get your horoscope:"
+$zodiacSign = Read-Host "Please enter the name name of the system for which a directory now is going to be created"
