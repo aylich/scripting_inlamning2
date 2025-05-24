29baa353e29820da8b86390bbe9dfedfd6a5f793
aylich <ayla.mahmutovic.student@ksgyf.se>
2025-05-24 15:20:56 +0200

    la till hashtabell samt ytterligare stats

--- Get-PokemonInfo.psm1
+++ Get-PokemonInfo.psm1
@@ -7,18 +7,29 @@ function Get-PokemonInfo {
     try {
         $response = Invoke-RestMethod -Uri $url -Method Get
 
+         # Extrahera statistik från API i en hashtabell
+        $stats = @{}
+        foreach ($stat in $response.stats) {
+            $statName = $stat.stat.name
+            $baseStat = $stat.base_stat
+            $stats[$statName] = $baseStat
+        }
+
         # Visa info om Pokémon i ett listformat (PSCustomObject)
         [PSCustomObject]@{
-            Name       = $response.name
-            ID         = $response.id
-            Types      = ($response.types | ForEach-Object { $_.type.name }) -join ", " # Specifiering av typ
-            Abilities  = ($response.abilities | ForEach-Object { $_.ability.name }) -join ", " # Specifiering
+            Name             = $response.name
+            ID               = $response.id
+            HP               = $stats["hp"]
+            Attack           = $stats["attack"]
+            Defense          = $stats["defense"]
+            'Sp. Attack'     = $stats["special-attack"]
+            'Sp. Defense'    = $stats["special-defense"]
+            Speed            = $stats["speed"]
+            Types            = ($response.types | ForEach-Object { $_.type.name }) -join ", " # Specifiering av typ
+            Abilities        = ($response.abilities | ForEach-Object { $_.ability.name }) -join ", "  # Specifiering
         }
     }
     catch {
         Write-Host "Can't show info. Check the name of the Pokémon or API-status."
     }
 }
-
-# Anropa funktionen - ska vara i ps1 och raderas härifrån:
-Get-PokemonInfo -pokemonName "pikachu"
